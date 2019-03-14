c10ec8369fe5a354b565ee3d447a8e350852fec2
Monika Jain <Monika.jain3088@gmail.com>
2019-03-14 03:28:57 +0100

    updated

--- BoardGames/ClientApp/src/app/Admin/adminvistordetails/adminvistordetails.component.ts
+++ BoardGames/ClientApp/src/app/Admin/adminvistordetails/adminvistordetails.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatTableDataSource, MatDialog, MatDialogRef } from '@angular/material';
+import { MatTableDataSource, MatDialog, MatDialogRef, MatSort, MatPaginator } from '@angular/material';
 import { AdminService } from 'src/app/shared/admin.service';
 
 @Component({
@@ -7,19 +7,24 @@ import { AdminService } from 'src/app/shared/admin.service';
   templateUrl: './adminvistordetails.component.html',
   styleUrls: ['./adminvistordetails.component.css']
 })
+
+  // class for visitor name and rating on click of 'visitor count' admin page
 export class AdminvistordetailsComponent implements OnInit {
   private rowdata:any;
-  public listData: MatTableDataSource<any>;  
+  public listData: MatTableDataSource<any>;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   public displayedColumns: string[] = ['visitorName','visitorRating'];
   constructor(private _adminService: AdminService,private dialog: MatDialog ,
     private dialogRef:MatDialogRef<AdminvistordetailsComponent>) { 
-    this.rowdata=this._adminService.rowData;
-    this.listData = new MatTableDataSource(this.rowdata);
+    
   }
 
-  
-
-  ngOnInit() {     
+  ngOnInit() {
+    this.rowdata = this._adminService.rowData;
+    this.listData = new MatTableDataSource(this.rowdata);
+    this.listData.sort = this.sort;
+    this.listData.paginator = this.paginator;
   }
   
   //Function to close the visitor details dialog
